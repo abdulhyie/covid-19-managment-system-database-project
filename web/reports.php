39766607a279be34788fe65ce90065f0d695a265
Abdul Hai <abdulhyyie@gmail.com>
2020-06-16 17:16:43 +0500

    Add files via upload

--- web/reports.php
+++ web/reports.php
@@ -19,7 +19,7 @@
 <!DOCTYPE html>
 <html>
 <head>
- <title>data extractor</title>
+ <title>Patients</title>
 </head>
 <body>
  <table>
@@ -62,3 +62,118 @@
 
 </body>
 </html>
+
+<?php
+
+  if(isset($_POST['isolation']))
+  {
+
+    $user = 'root';
+    $pass = "";
+    $database = "covid-19";
+    $host = 'localhost';
+
+    $con = mysqli_connect($host, $user, $pass, $database);
+
+    $query ="SELECT * FROM isolation_wards";
+
+    $records=mysqli_query($con,$query);
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Isolation Wards</title>
+</head>
+<body>
+ <table>
+  <tr>
+   <th>SNO</th>
+   <th>District</th>
+   <th>Province</th>
+   <th>Hospital</th>
+   <th>Ventilators</th>
+   <th>Beds</th>
+  </tr>
+
+ <?php
+
+ while($data=mysqli_fetch_assoc($records))
+ {
+
+  echo "<tr>";
+
+  echo "<td>".$data['SNO']."</td>";
+  echo "<td>".$data['district']."</td>";
+  echo "<td>".$data['province']."</td>";
+  echo "<td>".$data['hospital']."</td>";
+  echo "<td>".$data['ventilators']."</td>";
+  echo "<td>".$data['beds']."</td>";
+
+  echo "</tr>";
+
+
+   }
+ }
+?>   
+ </table>
+
+</body>
+</html>
+
+<?php
+
+  if(isset($_POST['quarantine']))
+  {
+
+    $user = 'root';
+    $pass = "";
+    $database = "covid-19";
+    $host = 'localhost';
+
+    $con = mysqli_connect($host, $user, $pass, $database);
+
+    $query ="SELECT * FROM quarantine_wards";
+
+    $records=mysqli_query($con,$query);
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Quarantine Wards</title>
+</head>
+<body>
+ <table>
+  <tr>
+   <th>SNO</th>
+   <th>Location</th>
+   <th>Province</th>
+   <th>Beds</th>
+  </tr>
+
+ <?php
+
+ while($data=mysqli_fetch_assoc($records))
+ {
+
+  echo "<tr>";
+
+  echo "<td>".$data['SNO']."</td>";
+  echo "<td>".$data['location']."</td>";
+  echo "<td>".$data['province']."</td>";
+  echo "<td>".$data['beds']."</td>";
+
+  echo "</tr>";
+
+
+   }
+ }
+?>   
+ </table>
+
+</body>
+</html>
+
